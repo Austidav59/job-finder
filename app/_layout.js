aebd88cfc2b533c41e68971732aec61a904a9147
Austidav59 <adavis10@byui.edu>
2024-12-05 19:24:51 -0600

    yes

--- app/_layout.js
+++ app/_layout.js
@@ -1,27 +1,30 @@
-import { Stack } from  "expo-router";
-import {useCallback} from 'react'
-import {useFonts} from 'expo-font'
-import * as SplashScreen from 'expo-splash-screen'
+import { Stack } from "expo-router";
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    const [fontsLoaded] = useFonts({
-        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
-        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
-        DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
-    })
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
+  });
 
-    const onLayoutRootView = useCallback(async ()=> {
-        if (fontsLoaded) {
-            await SplashScreen.hideAsync();
-        }
-    })
-
-    if (!fontsLoaded) {
-        return null
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
     }
-    return <Stack onLayout={onLayoutRootView}/>
-}
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return (
+    <Stack onLayout={onLayoutRootView}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+};
 
 export default Layout;
